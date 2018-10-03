10ebcf2892fba33f393a14f5e4dcad624d0fc391
Morisse <wigginsenders@gmail.com>
2018-10-03 16:32:59 +0200

    use header instead of json parameter for token access

--- synapse/rest/client/v1/watcha.py
+++ synapse/rest/client/v1/watcha.py
@@ -109,12 +109,9 @@ class WatchaRegisterRestServlet(ClientV1RestServlet):
     def on_POST(self, request):
         yield run_on_reactor() # not sure what it is :)
         auth_headers = request.requestHeaders.getRawHeaders("Authorization")
-        query_params = request.args.get("access_token")
         is_admin = False
-        logger.info('****************************************************************************')
-        logger.info(request.requestHeaders.getRawHeaders("Authorization"))
-        logger.info('****************************************************************************')
-        if query_params:
+
+        if auth_headers:
             requester = yield self.auth.get_user_by_req(request)
             is_admin = yield self.auth.is_server_admin(requester.user)
         logger.info("Adding Watcha user...")
