251b0c267e2848c82658bab9042e2ecfbb7822e5
Morisse <wigginsenders@gmail.com>
2019-05-17 11:44:59 +0200

    new txt template for onboarding

--- synapse/util/watcha_templates/invite_new_account.txt.py
+++ synapse/util/watcha_templates/invite_new_account.txt.py
@@ -1,12 +1,5 @@
-Bonjour,
+Bonjour,{{ inviter_name }} vous a invité à participer à l'espace de travail sécurisé Watcha {{ server }}.Connectez vous à Watcha (https://{{ server }}/#login/t={{ setupToken }}) 
 
-{{ inviter_name }} vous a invité à participer à l'espace de travail sécurisé Watcha {{ server }}.
-
-Votre nom d’utilisateur est :
-	{{ user_login }}
-
-Pour activer votre compte, cliquez ici:
-	https://{{ server }}/https://{{ server }}/#login/t={{ setupToken }}
-Nous pouvons vous aider à utiliser Watcha. Si vous rencontrez des difficultés, n'hésitez pas à répondre à cet email !
-
-L'équipe Watcha.
+ Pour vous reconnecter dans le futur, conservez cet email et utilisez le bouton de connection ci-dessus, ou notez vos identifiants de connection :Utilisateur :
+{{ user_login }}Serveur :
+{{ server }}N'hésitez pas à nous contacter en répondant cet email. L'équipe Watcha.
--- synapse/util/watcha_templates/new_account.txt.py
+++ synapse/util/watcha_templates/new_account.txt.py
@@ -2,11 +2,9 @@ Bonjour {{ full_name }},
 
 Vous êtes invité à participer à l’espace de travail sécurisé Watcha {{ server }}.
 
-Pour y accéder, votre nom d’utilisateur est :
-	{{ user_login }}
+Connectez vous à Watcha (https://{{ server }}/#login/t={{ setupToken }}) 
 
-Pour activer votre compte, cliquez ce lien:
-    https://{{ server }}/#login/t={{ setupToken }}
-Nous pouvons vous aider à utiliser Watcha. Si vous rencontrez des difficultés, n'hésitez pas à répondre à cet email !
+ Pour vous reconnecter dans le futur, conservez cet email et utilisez le bouton de connection ci-dessus, ou notez vos identifiants de connection :Utilisateur :
+{{ user_login }}Serveur :
+{{ server }}N'hésitez pas à nous contacter en répondant cet email. L'équipe Watcha.
 
-L'équipe Watcha.
