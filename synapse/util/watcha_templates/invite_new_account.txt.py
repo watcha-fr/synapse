e94c54fe5be70f124db508fa4605bfe4d17a86c8
Francois Granade <github.com@farialima.net>
2019-06-20 11:11:15 +0200

    small improvments to text emails

--- synapse/util/watcha_templates/invite_new_account.txt.py
+++ synapse/util/watcha_templates/invite_new_account.txt.py
@@ -1,5 +1,18 @@
-Bonjour,{{ inviter_name }} vous a invité à participer à l'espace de travail sécurisé Watcha {{ server }}.Connectez vous à Watcha (https://{{ server }}/#login/t={{ setupToken }}) 
+Bonjour,
 
- Pour vous reconnecter dans le futur, conservez cet email et utilisez le bouton de connection ci-dessus, ou notez vos identifiants de connection :Utilisateur :
-{{ user_login }}Serveur :
-{{ server }}N'hésitez pas à nous contacter en répondant cet email. L'équipe Watcha.
+{{ inviter_name }} vous a invité à participer à l'espace de travail sécurisé Watcha {{ server }}.
+
+Pour accepter cette invitation, connectez-vous à Watcha sur:
+
+      https://{{ server }}/#login/t={{ setupToken }}
+
+Pour vous reconnecter dans le futur, conservez cet email et utilisez le lien ci-dessus, ou notez vos identifiants de connection :
+
+Utilisateur : {{ user_login }}
+
+Serveur : {{ server }}
+
+N'hésitez pas à nous contacter en répondant cet email.
+
+
+L'équipe Watcha.
--- synapse/util/watcha_templates/new_account.txt.py
+++ synapse/util/watcha_templates/new_account.txt.py
@@ -2,9 +2,17 @@ Bonjour {{ full_name }},
 
 Vous êtes invité à participer à l’espace de travail sécurisé Watcha {{ server }}.
 
-Connectez vous à Watcha (https://{{ server }}/#login/t={{ setupToken }}) 
+Pour accepter cette invitation, connectez-vous à Watcha sur:
 
- Pour vous reconnecter dans le futur, conservez cet email et utilisez le bouton de connection ci-dessus, ou notez vos identifiants de connection :Utilisateur :
-{{ user_login }}Serveur :
-{{ server }}N'hésitez pas à nous contacter en répondant cet email. L'équipe Watcha.
+      https://{{ server }}/#login/t={{ setupToken }}
 
+Pour vous reconnecter dans le futur, conservez cet email et utilisez le lien ci-dessus, ou notez vos identifiants de connection :
+
+Utilisateur : {{ user_login }}
+
+Serveur : {{ server }}
+
+N'hésitez pas à nous contacter en répondant cet email.
+
+
+L'équipe Watcha.
