0f9abb790b6473df4a2d6f3af213197e3dfcbd5d
KevICO <kevin@watcha.fr>
2020-03-05 15:46:25 +0100

    creation of a test scenario for the new Servlet

---
+++ tests/rest/client/v1/test_watcha_register_threepids.py
@@ -0,0 +1,45 @@
+from tests import unittest
+from tests.utils import setup_test_homeserver
+from synapse.rest.client.v1 import watcha, login
+from synapse.rest import admin
+import json
+
+class WatchaRegisterThreePidServletTestCase(unittest.HomeserverTestCase):
+    
+    servlets = [
+        admin.register_servlets_for_client_rest_resource,
+        login.register_servlets,
+        watcha.register_servlets
+    ]
+
+    def prepare(self, reactor, clock, hs):
+        # User register.
+        self.user_id = self.register_user("user_test", "pass")
+        self.user_access_token = self.login("user_test", "pass")
+
+    def _do_register_threepids(self, request_content):
+        request, channel = self.make_request(
+            "POST",
+            "/_matrix/client/r0/watcha_threepids",
+            content=json.dumps(request_content),
+            access_token=self.user_access_token,
+        )
+        self.render(request)
+        return channel.code
+
+    def test_right_register_threepids(self):
+        request_content = {"user":self.user_id,"email":"test@test.com"}
+        code = self._do_register_threepids(request_content)
+        self.assertEqual(code, 200)
+
+    #Tester le retour du signal 200 (user = requester_user) : 
+
+    #Tester le retour du signal 400 (user != requester_user) : 
+
+    #Tester le formatage du user_id (si user_id ne commance pas par @):
+
+    #Tester si l'email renseigné = email recupéré dans requester : 
+
+
+
+    
