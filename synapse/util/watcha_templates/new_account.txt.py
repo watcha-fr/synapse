2497ad9f62dde1c30139459fa5f9255d5f4dd29a
c-cal <github-2c7c@zebrina.fr>
2020-03-24 16:38:28 +0100

    fix: typo & improve text layout

--- synapse/util/watcha_templates/new_account.txt.py
+++ synapse/util/watcha_templates/new_account.txt.py
@@ -1,13 +1,14 @@
 Bonjour{{ full_name }},
 
-{{inviter_name}} vous a invité(e) à rejoindre l'espace de travail sécurisé Watcha
+{{ inviter_name }} vous a invité(e) à rejoindre l'espace de travail sécurisé Watcha
 Vous pourrez y créer et utiliser des salons de discussion avec les autres utilisateurs.
 
 Pour vous connecter à Watcha, suivez ce lien :
 
       {{ login_url }}
 
-Pour vous reconnecter dans le futur, conservez cet email et utilisez le lien ci-dessus, ou notez vos identifiants de connection :
+Pour vous reconnecter dans le futur, conservez cet email et utilisez le lien ci-dessus, ou notez vos identifiants de connexion.
+
 
 Utilisateur : {{ user_login }}
 
@@ -15,7 +16,7 @@ Mot de passe : à définir lors de la première connection
 
 Serveur : {{ server }}
 
-N'hésitez pas à nous contacter en répondant cet email.
 
+N'hésitez pas à nous contacter en répondant cet email.
 
 L'équipe Watcha.
