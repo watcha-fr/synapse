8420ddbaa637f13cf4b25e98acad24036280d1a5
KevICO <kevin.icol@hotmail.fr>
2020-03-30 09:16:37 +0200

    style(synaspe-test): deletion of not used variable.

--- tests/util/test_watcha_util.py
+++ tests/util/test_watcha_util.py
@@ -5,22 +5,13 @@ import base64
 class WatchaUtilTestCase(unittest.HomeserverTestCase):
     
     def test_compute_registration_token_without_email_and_password(self):
-        user = "user_test"
-
-        token = compute_registration_token(user)
+        token = compute_registration_token("user_test")
         self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test"}')
 
     def test_compute_registration_token_without_password(self):
-        user = "user_test"
-        email = "test@email.com"
-
-        token = compute_registration_token(user, email)
+        token = compute_registration_token("user_test", "test@email.com")
         self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test", "email":"test@email.com"}')
 
     def test_compute_registration_token(self):
-        user = "user_test"
-        password = "password"
-        email = "test@email.com"
-
-        token = compute_registration_token(user, email, password)
+        token = compute_registration_token("user_test", "test@email.com", "password")
         self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test", "email":"test@email.com", "pw":"password"}')
