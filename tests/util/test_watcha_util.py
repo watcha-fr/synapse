ba758c504cbdbee140bfe79bac7b0cfd2c528084
KevICO <kevin.icol@hotmail.fr>
2020-04-06 16:56:27 +0200

    Revert "feat:modification of code afer review on PR#46."
    This reverts commit 47e30fd3e8a0dc1256e91b1a49e18e78dbdcc05e.

--- tests/util/test_watcha_util.py
+++ tests/util/test_watcha_util.py
@@ -27,15 +27,15 @@ class WatchaUtilTestCase(unittest.HomeserverTestCase):
 
     def test_compute_registration_token_without_email_and_password(self):
         token = compute_registration_token("user_test")
-        self.assertEquals(base64.b64decode(token).decode(),'{"user":"user_test"}')
+        self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test"}')
 
     def test_compute_registration_token_without_password(self):
         token = compute_registration_token("user_test", "test@email.com")
-        self.assertEquals(base64.b64decode(token).decode(),'{"user":"user_test", "email":"test@email.com"}')
+        self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test", "email":"test@email.com"}')
 
     def test_compute_registration_token(self):
         token = compute_registration_token("user_test", "test@email.com", "password")
-        self.assertEquals(base64.b64decode(token).decode(),'{"user":"user_test", "email":"test@email.com", "pw":"password"}')
+        self.assertEquals(base64.b64decode(token).decode('ascii'),'{"user":"user_test", "email":"test@email.com", "pw":"password"}')
 
     def test_create_display_inviter_name_with_email(self):
         self.auth_handler.add_threepid(self.owner, "email", "example@email.com", self.time)
