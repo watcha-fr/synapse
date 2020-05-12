de50b179e97fae3234084b64a722f111b1cee5db
KevICO <60855408+KevICO@users.noreply.github.com>
2020-05-12 09:35:58 +0200

    Watcha op313 modify user page on admin dashboard (#61)
    * feat: modification of get_user_admin function.
    
    * feat: make get_user_admin private.
    
    * small fix to function names, and added test
    
    * feat: revert WatchaAdminStatsRestServletTestCase?
    
    * style: rename test_watcha_register.py to test_watcha.py.
    
    Co-authored-by: Francois Granade <github.com@farialima.net>

--- tests/rest/client/v1/test_watcha.py
+++ tests/rest/client/v1/test_watcha.py
@@ -1,10 +1,11 @@
+import json
+import logging
+
 from tests import unittest
 from tests.utils import setup_test_homeserver
 from synapse.rest.client.v1 import watcha, login, room
 from synapse.rest import admin
 from synapse.api.errors import SynapseError
-import json
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +15,6 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
         admin.register_servlets_for_client_rest_resource,
         login.register_servlets,
         watcha.register_servlets,
-        room.register_servlets,
     ]
 
     def make_homeserver(self, reactor, clock):
@@ -29,7 +29,6 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
             },
             "public_baseurl": "TEST"
         })
-        self.account_activity_handler = self.hs.get_account_validity_handler()
 
         return self.hs
 
@@ -48,37 +47,6 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
         )
         self.render(request)
         return channel
-
-    def _do_update_email(self, user_id, email):
-        #Admin send the request with access_token :
-        request, channel = self.make_request(
-            "PUT",
-            "/_matrix/client/r0/watcha_update_email/%s" % user_id,
-            content=json.dumps({"new_email": email}),
-            access_token=self.user_access_token,
-        )
-        self.render(request)
-        return channel
-    def _create_room(self):
-        request, channel = self.make_request(
-            "POST",
-            "/createRoom",
-            content=json.dumps({}),
-            access_token=self.user_access_token,
-        )
-
-        self.render(request)
-        return json.loads(channel.result["body"])["room_id"]
-
-    def _invite_member_in_room(self, room_id, user_id):
-        request, channel = self.make_request(
-            "POST",
-            "/rooms/%s/invite" % room_id,
-            content=json.dumps({"user_id": user_id}),
-            access_token=self.user_access_token,
-        )
-
-        self.render(request)
 
 
 class WatchaRegisterRestServletTestCase(BaseHomeserverWithEmailTestCase):
@@ -144,158 +112,3 @@ class WatchaResetPasswordRestServletTestCase(BaseHomeserverWithEmailTestCase):
             self.assertIn("http://localhost:8080/setup-account.html?t=",
                             ''.join(cm.output))
             self.assertEqual(channel.code,200)
-
-class WatchaUpdateUserRoleResterServletTestCase(BaseHomeserverWithEmailTestCase):
-    def prepare(self, reactor, clock, hs):
-        # Register admin user :
-        self.admin_id = self.register_user("admin_user", "pass", True)
-        self.admin_access_token = self.login("admin_user", "pass")
-
-        # Register no admin user
-        self.non_admin_id = self.register_user("no_admin_user", "pass", False)
-        self.non_admin_user_token = self.login("no_admin_user", "pass")
-
-    def _do_update_user_role(self, target_user_id, access_token, role):
-        request, channel = self.make_request(
-            "PUT",
-            "/_matrix/client/r0/watcha_update_user_role/%s" % target_user_id,
-            content=json.dumps({"role": role}),
-            access_token=access_token,
-        )
-        self.render(request)
-        return channel
-
-    def test_do_all_update_user_role(self):
-        roles = ["partner", "member", "admin"]
-
-        for role in roles:
-            channel = self._do_update_user_role(
-                self.non_admin_id, self.admin_access_token, role
-            )
-
-            self.assertEqual(channel.code, 200)
-            self.assertEqual(json.loads(channel.result["body"]), {"new_role": role})
-
-    def test_do_update_user_role_on_ourself(self):
-        role = "member"
-        channel = self._do_update_user_role(
-            self.admin_id, self.admin_access_token, role
-        )
-
-        self.assertEqual(channel.code, 200)
-        self.assertEqual(json.loads(channel.result["body"]), {"new_role": role})
-
-    def test_do_update_user_role_with_same_role(self):
-        role = "member"
-        channel = self._do_update_user_role(
-            self.non_admin_id, self.admin_access_token, role
-        )
-
-        self.assertRaises(SyntaxError)
-        self.assertEqual(channel.code, 400)
-        self.assertEqual(
-            json.loads(channel.result["body"])["error"],
-            "This user has already the %s role" % role,
-        )
-
-    def test_do_update_user_role_without_admin_right(self):
-        channel = self._do_update_user_role(
-            self.admin_id, self.non_admin_user_token, "admin"
-        )
-
-        self.assertRaises(SyntaxError)
-        self.assertEqual(channel.code, 403)
-        self.assertEqual(
-            json.loads(channel.result["body"])["error"], "You are not a server admin"
-        )
-
-    def test_do_update_user_role_with_wrong_user_id(self):
-        channel = self._do_update_user_role(
-            "@user_test:test", self.admin_access_token, "admin"
-        )
-
-        self.assertRaises(SyntaxError)
-
-    def test_do_update_user_role_with_wrong_role(self):
-        role = "master"
-        channel = self._do_update_user_role(
-            self.non_admin_id, self.admin_access_token, role
-        )
-
-        self.assertRaises(SyntaxError)
-        self.assertEqual(channel.code, 400)
-        self.assertEqual(
-            json.loads(channel.result["body"])["error"],
-            "%s is not a defined role." % role,
-        )
-
-
-class WatchaUpdateMailRestServletTestCase(BaseHomeserverWithEmailTestCase):
-    def test_update_email(self):
-        user_id = "@user_test:test"
-        self._do_register_user(
-            {
-                "user": "user_test",
-                "full_name": "test",
-                "email": "example@example.com",
-                "admin": False,
-            }
-        )
-
-        self._do_update_email(user_id, "example2@example.com")
-        self.assertEqual(
-            self.get_success(
-                self.account_activity_handler.get_email_address_for_user(user_id)
-            ),
-            "example2@example.com",
-        )
-
-    def test_update_email_when_a_email_is_not_defined(self):
-        self.register_user("user_test", "pass")
-
-        with self.assertLogs("synapse.rest.client.v1.watcha", level="ERROR") as cm:
-            channel = self._do_update_email("@user_test:test", "example@example.com")
-
-        self.assertEqual(channel.code, 403)
-        self.assertIn(
-            "ERROR:synapse.rest.client.v1.watcha:No email are defined for this user.",
-            cm.output[0],
-        )
-
-    def test_update_email_with_no_new_email_parameter(self):
-        channel = self._do_update_email("@user_test:test", None)
-        self.assertRaises(SynapseError)
-        self.assertEqual(channel.code, 400)
-        self.assertEqual(
-            json.loads(channel.result["body"])["error"], "Missing 'new_email' arg"
-        )
-
-    def test_update_email_with_wrong_target_user_id(self):
-        channel = self._do_update_email("@user_test:test", "example@example.com")
-        self.assertRaises(SynapseError)
-        self.assertEqual(channel.code, 400)
-        self.assertEqual(
-            json.loads(channel.result["body"])["error"],
-            "The target user is not registered in this homeserver.",
-        )
-
-class WatchaAdminStatsTestCase(BaseHomeserverWithEmailTestCase):
-    def test_get_watcha_admin_stats_room_type(self):
-        room_id = self._create_room()
-        self._invite_member_in_room(room_id, self.user_id)
-
-        request, channel = self.make_request(
-            "GET", "watcha_admin_stats", access_token=self.user_access_token
-        )
-        self.render(request)
-
-        self.assertEquals(
-            json.loads(channel.result["body"])["rooms"],
-            {
-                "direct_active_rooms_count": 0,
-                "direct_rooms_count": 0,
-                "non_direct_active_rooms_count": 0,
-                "non_direct_rooms_count": 1,
-            },
-        )
-        self.assertEquals(200, channel.code)
