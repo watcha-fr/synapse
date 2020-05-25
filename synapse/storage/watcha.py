11061ab905b2934a3b74ee3416aeabbfc4e56af7
KevICO <60855408+KevICO@users.noreply.github.com>
2020-05-25 09:31:55 +0200

    Watcha op239 delete datas of old setup email process (#66)
    * feat: deletion of _add_column_if_needed for 'email' in users table.
    
    * feat: creation of _drop_column_if_needed.
    
    * style: spelling correction of word 'dropped'.
    
    * feat: deletion of useless column_details parameter.
    
    * feat: string insertion in SQL queries.
    
    * style: format code with black.
    
    * feat: string insertion for DROP SQL query.
    
    * feat: use better code syntax in _drop_column_if_needed function.
    
    * feat: add a warning on _drop_column_if_needed function for default string values.
    
    * Refactor: add some intermediate variable for more readability.
    
    * feat: change variable name 'cur' by 'cursor'.
    
    * refactor: change syntax code for more readability.

--- synapse/storage/watcha.py
+++ synapse/storage/watcha.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 def check_db_customization(db_conn, database_engine):
     # put here the list of db customizations
     # (database_engine will be used later to for postgres)
+    _drop_column_if_needed(db_conn, "users", "users_copy", "email")
     _add_column_if_needed(db_conn, "users", "is_partner", "DEFAULT 0")
-    _add_column_if_needed(db_conn, "users", "email", "TEXT")
     _add_column_if_needed(db_conn, "users", "is_active", "DEFAULT 1")
     _add_table_partners_invited_by(db_conn)
 
@@ -20,12 +20,82 @@ def _add_column_if_needed(db_conn, table, column, column_details):
         cur = db_conn.cursor()
         cur.execute("PRAGMA table_info(%s);" % table)
         if column not in [row[1] for row in cur.fetchall()]:
-            cur.execute("ALTER TABLE %s ADD COLUMN %s %s;" % (table, column, column_details))
-            logger.info("check_db_customization: column %s added to table %s", column, table)
+            cur.execute(
+                "ALTER TABLE %s ADD COLUMN %s %s;" % (table, column, column_details)
+            )
+            logger.info(
+                "check_db_customization: column %s added to table %s", column, table
+            )
         else:
-            logger.info("check_db_customization: column %s.%s already present", column, table)
+            logger.info(
+                "check_db_customization: column %s.%s already present", column, table
+            )
     except:
-        logger.warn("check_db_customization: could not check %s.%s column", column, table)
+        logger.warn(
+            "check_db_customization: could not check %s.%s column", column, table
+        )
+        db_conn.rollback()
+        raise
+
+def _drop_column_if_needed(db_conn, table, copy_table, column_to_drop):
+    """ WARNING : if the table has default string value, PLEASE TEST as it may remove this default value."""
+
+    try:
+        cursor = db_conn.cursor()
+        cursor.execute("PRAGMA table_info({});".format(table))
+        columns = {
+            row[1]: " ".join(
+                [
+                    row[1],
+                    row[2],
+                    "DEFAULT " + row[4] if row[4] else "",
+                    "NOT NULL" if row[3] else "",
+                    "PRIMARY KEY" if row[5] else "",
+                ]
+            )
+            for row in cursor.fetchall()
+        }
+
+        if column_to_drop not in columns:
+            logger.info(
+                "check_db_customization: column %s. Already dropped from %s",
+                column_to_drop,
+                table,
+            )
+            return
+
+        del columns[column_to_drop]
+
+        sql_script = """
+            BEGIN TRANSACTION;
+            CREATE TABLE IF NOT EXISTS {new_table} (
+                {columns_definition}
+            , UNIQUE(name));
+            INSERT INTO {new_table}({columns_name})
+            SELECT {columns_name}
+            FROM users;
+            DROP TABLE {old_table};
+            ALTER TABLE {new_table} RENAME TO {old_table};
+        """.format(
+            new_table=copy_table, columns_definition=",".join(columns.values()), columns_name=", ".join(columns.keys()), old_table=table
+        )
+
+        for line in sql_script.split(";"):
+            cursor.execute(line)
+
+        db_conn.commit()
+
+        logger.info(
+            "check_db_customization: column %s dropped to table %s",
+            column_to_drop,
+            table,
+        )
+    except:
+        logger.warn(
+            "check_db_customization: could not check %s.%s column",
+            column_to_drop,
+            table,
+        )
         db_conn.rollback()
         raise
 
