1bbf96d9b420c13b92a9619e0d10c84773002309
Kevin ICOL <kevin.icol@hotmail.fr>
2020-06-30 10:23:36 +0200

    fix: better affectation of 'statut' value

--- synapse/storage/watcha_admin.py
+++ synapse/storage/watcha_admin.py
@@ -436,17 +436,19 @@ class WatchaAdminStore(SQLBaseStore):
     def watcha_get_user_status(self, user_id):
         users_with_pending_invitation = yield self._get_users_with_pending_invitation()
 
-        if user_id in users_with_pending_invitation:
-            result = "invited"
-        else:
-            is_active = yield self._simple_select_onecol(
-                table="users", keyvalues={"name": user_id}, retcol="is_active",
-            )
+        is_active = yield self._simple_select_onecol(
+            table="users", keyvalues={"name": user_id}, retcol="is_active",
+        )
 
-            result = "inactive" if is_active[0] == 0 else "active"
+        result = (
+            "inactive"
+            if is_active[0] == 0
+            else "invited"
+            if user_id in users_with_pending_invitation
+            else "active"
+        )
 
         defer.returnValue(result)
-
 
     @defer.inlineCallbacks
     def _get_users_with_pending_invitation(self):
