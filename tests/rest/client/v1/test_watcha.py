58134b00a0f4bb30373e9793ec8869ad6a4bd687
KevICO <kevin.icol@hotmail.fr>
2020-07-24 18:16:20 +0200

    black: format test_watcha with black

--- tests/rest/client/v1/test_watcha.py
+++ tests/rest/client/v1/test_watcha.py
@@ -9,6 +9,7 @@ from synapse.api.errors import SynapseError
 
 logger = logging.getLogger(__name__)
 
+
 class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
 
     servlets = [
@@ -19,17 +20,19 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
     ]
 
     def make_homeserver(self, reactor, clock):
-        self.hs = self.setup_test_homeserver(config={
-            **self.default_config(),
-            "require_auth_for_profile_requests": True,
-            "email": {
-                "riot_base_url": "http://localhost:8080",
-                "smtp_host": "TEST",
-                "smtp_port": 10,
-                "notif_from": "TEST"
-            },
-            "public_baseurl": "TEST"
-        })
+        self.hs = self.setup_test_homeserver(
+            config={
+                **self.default_config(),
+                "require_auth_for_profile_requests": True,
+                "email": {
+                    "riot_base_url": "http://localhost:8080",
+                    "smtp_host": "TEST",
+                    "smtp_port": 10,
+                    "notif_from": "TEST",
+                },
+                "public_baseurl": "TEST",
+            }
+        )
         self.auth = self.hs.get_auth_handler()
 
         return self.hs
@@ -42,7 +45,7 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
         self.auth.add_threepid(self.user_id, "email", "example@email.com", self.time)
 
     def _do_register_user(self, request_content):
-        #Admin send the request with access_token :
+        # Admin send the request with access_token :
         request, channel = self.make_request(
             "POST",
             "/_matrix/client/r0/watcha_register",
@@ -75,79 +78,136 @@ class BaseHomeserverWithEmailTestCase(unittest.HomeserverTestCase):
 
 
 class WatchaRegisterRestServletTestCase(BaseHomeserverWithEmailTestCase):
-
     def test_register_user(self):
-        with self.assertLogs('synapse.util.watcha', level='INFO') as cm:
-            request_content = {"user":"user_test", "full_name":"test", "email":"test@test.com", "admin":False, "password": ""}
+        with self.assertLogs("synapse.util.watcha", level="INFO") as cm:
+            request_content = {
+                "user": "user_test",
+                "full_name": "test",
+                "email": "test@test.com",
+                "admin": False,
+                "password": "",
+            }
             channel = self._do_register_user(request_content)
-            self.assertIn("INFO:synapse.util.watcha:NOT Sending registration email to \'test@test.com\', we are in test mode",
-                            ''.join(cm.output))
-            self.assertIn(" http://localhost:8080/#/login/t=",
-                            ''.join(cm.output))
-            self.assertEqual(channel.result['body'], b'{"display_name":"test","user_id":"@user_test:test"}')
-            self.assertEqual(channel.code,200)
+            self.assertIn(
+                "INFO:synapse.util.watcha:NOT Sending registration email to 'test@test.com', we are in test mode",
+                "".join(cm.output),
+            )
+            self.assertIn(" http://localhost:8080/#/login/t=", "".join(cm.output))
+            self.assertEqual(
+                channel.result["body"],
+                b'{"display_name":"test","user_id":"@user_test:test"}',
+            )
+            self.assertEqual(channel.code, 200)
 
     def test_register_user_with_password(self):
-        request_content = {"user":"user_test", "full_name":"test", "email":"test@test.com", "admin":False, "password": "password"}
+        request_content = {
+            "user": "user_test",
+            "full_name": "test",
+            "email": "test@test.com",
+            "admin": False,
+            "password": "password",
+        }
         channel = self._do_register_user(request_content)
-        self.assertEqual(channel.result['body'], b'{"display_name":"test","user_id":"@user_test:test"}')
-        self.assertEqual(channel.code,200)
+        self.assertEqual(
+            channel.result["body"],
+            b'{"display_name":"test","user_id":"@user_test:test"}',
+        )
+        self.assertEqual(channel.code, 200)
 
     def test_register_user_with_upper_user_id(self):
-        request_content = {"user":"USER_TEST", "full_name":"test", "email":"test@test.com", "admin":False, "password": ""}
+        request_content = {
+            "user": "USER_TEST",
+            "full_name": "test",
+            "email": "test@test.com",
+            "admin": False,
+            "password": "",
+        }
         channel = self._do_register_user(request_content)
-        self.assertEqual(channel.code,500)
+        self.assertEqual(channel.code, 500)
 
     def test_register_user_with_empty_email(self):
-        request_content = {"user":"user_test", "full_name":"test", "email":"", "admin":False, "password": ""}
+        request_content = {
+            "user": "user_test",
+            "full_name": "test",
+            "email": "",
+            "admin": False,
+            "password": "",
+        }
         channel = self._do_register_user(request_content)
-        self.assertEqual(channel.code,500)
+        self.assertEqual(channel.code, 500)
 
     def test_register_user_with_same_email_adress(self):
-        request_content = {"user":"user_test", "full_name":"test", "email":"test@test.com", "admin":False, "password": ""}
+        request_content = {
+            "user": "user_test",
+            "full_name": "test",
+            "email": "test@test.com",
+            "admin": False,
+            "password": "",
+        }
         self._do_register_user(request_content)
-        request_content = {"user":"other_user", "full_name":"other", "email":"test@test.com", "admin":False, "password": ""}
+        request_content = {
+            "user": "other_user",
+            "full_name": "other",
+            "email": "test@test.com",
+            "admin": False,
+            "password": "",
+        }
         channel = self._do_register_user(request_content)
-        self.assertEqual(channel.code,500)
+        self.assertEqual(channel.code, 500)
 
     def test_register_user_with_plus_in_email(self):
-        with self.assertLogs('synapse.util.watcha', level='INFO') as cm:
-            request_content = {"user":"user_test", "full_name":"test", "email":"test+test@test.com", "admin":False, "password": ""}
+        with self.assertLogs("synapse.util.watcha", level="INFO") as cm:
+            request_content = {
+                "user": "user_test",
+                "full_name": "test",
+                "email": "test+test@test.com",
+                "admin": False,
+                "password": "",
+            }
             channel = self._do_register_user(request_content)
-            self.assertIn("INFO:synapse.util.watcha:NOT Sending registration email to \'test+test@test.com\', we are in test mode",
-                            ''.join(cm.output))
-            self.assertIn(" http://localhost:8080/#/login/t=",
-                            ''.join(cm.output))
-            self.assertEqual(channel.result['body'], b'{"display_name":"test","user_id":"@user_test:test"}')
-            self.assertEqual(channel.code,200)
+            self.assertIn(
+                "INFO:synapse.util.watcha:NOT Sending registration email to 'test+test@test.com', we are in test mode",
+                "".join(cm.output),
+            )
+            self.assertIn(" http://localhost:8080/#/login/t=", "".join(cm.output))
+            self.assertEqual(
+                channel.result["body"],
+                b'{"display_name":"test","user_id":"@user_test:test"}',
+            )
+            self.assertEqual(channel.code, 200)
 
 
 class WatchaResetPasswordRestServletTestCase(BaseHomeserverWithEmailTestCase):
-
     def test_reset_password(self):
-        self._do_register_user({"user":"user_test",
-                                "full_name":"test",
-                                "email":"test@test.com",
-                                "admin": False,
-                                 "password": "password"})
-        with self.assertLogs('synapse.util.watcha', level='INFO') as cm:
+        self._do_register_user(
+            {
+                "user": "user_test",
+                "full_name": "test",
+                "email": "test@test.com",
+                "admin": False,
+                "password": "password",
+            }
+        )
+        with self.assertLogs("synapse.util.watcha", level="INFO") as cm:
             request, channel = self.make_request(
                 "POST",
                 "/_matrix/client/r0/watcha_reset_password",
-                content=json.dumps({ "user": "user_test" }),
+                content=json.dumps({"user": "user_test"}),
                 access_token=self.user_access_token,
             )
             self.render(request)
 
-            self.assertIn("INFO:synapse.util.watcha:NOT Sending registration email to \'test@test.com\', we are in test mode",
-                            ''.join(cm.output))
-            self.assertIn("http://localhost:8080/setup-account.html?t=",
-                            ''.join(cm.output))
-            self.assertEqual(channel.code,200)
+            self.assertIn(
+                "INFO:synapse.util.watcha:NOT Sending registration email to 'test@test.com', we are in test mode",
+                "".join(cm.output),
+            )
+            self.assertIn(
+                "http://localhost:8080/setup-account.html?t=", "".join(cm.output)
+            )
+            self.assertEqual(channel.code, 200)
 
 
 class WatchaAdminStatsTestCase(BaseHomeserverWithEmailTestCase):
-
     def test_watcha_user_list(self):
         self._do_register_user(
             {
@@ -160,7 +220,9 @@ class WatchaAdminStatsTestCase(BaseHomeserverWithEmailTestCase):
         )
 
         request, channel = self.make_request(
-            "POST", "admin/deactivate/@user_test:test", access_token=self.user_access_token,
+            "POST",
+            "admin/deactivate/@user_test:test",
+            access_token=self.user_access_token,
         )
         self.render(request)
 
@@ -192,39 +254,43 @@ class WatchaAdminStatsTestCase(BaseHomeserverWithEmailTestCase):
                 },
             ],
         )
-    
+
     def test_get_watcha_admin_user_stats(self):
-        self._do_register_user({"user":"user_test",
-                                "full_name":"test",
-                                "email":"test@test.com",
-                                "admin": False,
-                                "password": "" })
+        self._do_register_user(
+            {
+                "user": "user_test",
+                "full_name": "test",
+                "email": "test@test.com",
+                "admin": False,
+                "password": "",
+            }
+        )
 
         request, channel = self.make_request(
-                "GET",
-                "/watcha_admin_stats",
-                access_token=self.user_access_token,
+            "GET", "/watcha_admin_stats", access_token=self.user_access_token,
         )
         self.render(request)
         self.assertEquals(
             json.loads(channel.result["body"])["users"],
             {
-                "administrators_users": [{"displayname": None,
-                    "email": "example@email.com",
-                    "user_id": "@admin:test"}],
+                "administrators_users": [
+                    {
+                        "displayname": None,
+                        "email": "example@email.com",
+                        "user_id": "@admin:test",
+                    }
+                ],
                 "users_per_role": {
                     "administrators": 1,
                     "collaborators": 1,
                     "partners": 0,
                 },
                 "connected_users": {
-                    "number_of_users_logged_at_least_once":0,
-                    "number_of_last_month_logged_users":0,
+                    "number_of_users_logged_at_least_once": 0,
+                    "number_of_last_month_logged_users": 0,
                     "number_of_last_week_logged_users": 0,
                 },
-                "other_statistics": {
-                    "number_of_users_with_pending_invitation": 2,
-                },
+                "other_statistics": {"number_of_users_with_pending_invitation": 2,},
             },
         )
 
