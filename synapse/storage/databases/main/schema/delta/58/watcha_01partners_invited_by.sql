d451f4d3a15c8cbc9e3d136ed74f469fcdf188bf
KevICO <60855408+KevICO@users.noreply.github.com>
2020-10-07 09:58:15 +0200

    Update synapse/storage/databases/main/schema/delta/58/watcha_01partners_invited_by.sql
    Co-authored-by: c-cal <57274151+c-cal@users.noreply.github.com>

--- synapse/storage/databases/main/schema/delta/58/watcha_01partners_invited_by.sql
+++ synapse/storage/databases/main/schema/delta/58/watcha_01partners_invited_by.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS partners_invited_by(
+CREATE TABLE IF NOT EXISTS partners_invited_by (
     partner TEXT NOT NULL, -- The user_id created for the partner.
     invited_by TEXT NOT NULL, -- The user_id of the inviter.
     invitation_ts BIGINT NOT NULL,
