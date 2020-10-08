51d61c44208ab36538f3cdbd969471f1b88eece4
KevICO <60855408+KevICO@users.noreply.github.com>
2020-10-08 09:48:29 +0200

    Watcha op529 fix partner can change password on sso (#97)
    * feat: adds posibility to register a partner in test mode
    
    * fix: authorize partner user to get capabalities
    
    * feat: adds Watcha tags
    
    * test: adds test for room version capabilities with partner

--- tests/unittest.py
+++ tests/unittest.py
@@ -462,7 +462,10 @@ class HomeserverTestCase(TestCase):
         self.pump()
         return self.failureResultOf(d, exc)
 
+    """ watcha!
     def register_user(self, username, password, admin=False):
+    !watcha """
+    def register_user(self, username, password, admin=False, is_partner=False): # watcha+
         """
         Register a user. Requires the Admin API be registered.
 
@@ -501,6 +504,7 @@ class HomeserverTestCase(TestCase):
                 "admin": admin,
                 "mac": want_mac,
                 "inhibit_login": True,
+                "is_partner": False if admin else is_partner, # watcha+
             }
         )
         request, channel = self.make_request(
