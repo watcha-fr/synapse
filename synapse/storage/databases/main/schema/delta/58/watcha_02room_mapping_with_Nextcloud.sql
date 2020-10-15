ab89dbe01d1d543ef86f497462c8a1ee51ddff54
KevICO <60855408+KevICO@users.noreply.github.com>
2020-10-15 16:45:05 +0200

    Watcha op607 rename room mapping with nc table (#102)
    * feat: change table name of room_mapping_with_NC
    
    * feat: change table name of room_mapping_with_NC in code
    
    * Update tests/storage/test_room.py
    
    Co-authored-by: c-cal <57274151+c-cal@users.noreply.github.com>
    
    Co-authored-by: c-cal <57274151+c-cal@users.noreply.github.com>

--- synapse/storage/databases/main/schema/delta/58/watcha_02room_mapping_with_Nextcloud.sql
+++ synapse/storage/databases/main/schema/delta/58/watcha_02room_mapping_with_Nextcloud.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS room_mapping_with_NC (
+CREATE TABLE IF NOT EXISTS room_nextcloud_mapping (
     room_id TEXT NOT NULL PRIMARY KEY, -- The room ID of the room which initiate a share with Nextcloud.
     directory_path TEXT -- The Nextcloud directory path to share in the room. 
 );
