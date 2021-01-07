463bdca897170ce99c3ffa0a9f4f934f54e9fde8
Kevin ICOL <kevin.icol@hotmail.fr>
2021-01-07 15:38:16 +0100

    feat: remove unused watcha_deactivate_account

--- synapse/storage/databases/main/watcha_admin.py
+++ synapse/storage/databases/main/watcha_admin.py
@@ -496,9 +496,6 @@ class AdministrationStore(SQLBaseStore):
         elif role == "partner":
             return await self._update_user(user_id, admin=0, is_partner=1)
 
-    async def watcha_deactivate_account(self, user_id):
-        return await self._update_user(user_id, is_active=0)
-
     async def _get_users_with_pending_invitation(self):
         """Retrieve a list of users who have a pending invitation.
 
