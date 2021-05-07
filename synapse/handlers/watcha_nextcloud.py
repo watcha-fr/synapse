e3f977f17f6a20b64193bb4869e38412ce23162c
KevICO <kevin@watcha.fr>
2021-05-07 11:10:41 +0200

    fix: add space between group prefix and room name

--- synapse/handlers/watcha_nextcloud.py
+++ synapse/handlers/watcha_nextcloud.py
@@ -91,7 +91,7 @@ class NextcloudHandler(BaseHandler):
             room_id: the id of the room
         """
         room_name = await self.administration_handler.calculate_room_name(room_id)
-        group_displayname = self.group_displayname_prefix + room_name
+        group_displayname = f"{self.group_displayname_prefix} {room_name}"
         return group_displayname
 
     async def set_group_displayname(self, group_id: str, group_displayname: str):
