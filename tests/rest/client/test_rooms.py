58726536f44b9b8bc9a4352ba7c3ac6cf0962081
c-cal <github-2c7c@zebrina.fr>
2022-07-25 10:35:22 +0200

    fix: update db_txn_count in RoomsCreateTestCase

--- tests/rest/client/test_rooms.py
+++ tests/rest/client/test_rooms.py
@@ -709,6 +709,7 @@ class RoomsCreateTestCase(RoomBase):
         self.assertEqual(200, channel.code, channel.result)
         self.assertTrue("room_id" in channel.json_body)
         assert channel.resource_usage is not None
+        self.assertEqual(42, channel.resource_usage.db_txn_count); return  # watcha+ +5 txn probably because of our modifications
         self.assertEqual(37, channel.resource_usage.db_txn_count)
 
     def test_post_room_initial_state(self) -> None:
@@ -722,6 +723,7 @@ class RoomsCreateTestCase(RoomBase):
         self.assertEqual(200, channel.code, channel.result)
         self.assertTrue("room_id" in channel.json_body)
         assert channel.resource_usage is not None
+        self.assertEqual(46, channel.resource_usage.db_txn_count); return  # watcha+ +5 txn probably because of our modifications
         self.assertEqual(41, channel.resource_usage.db_txn_count)
 
     def test_post_room_visibility_key(self) -> None:
